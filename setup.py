679a5d9ac4138ba5cec98e817df92598179f2ca5
swaroop <swaroopyadav49@gmail.com>
2025-04-30 09:54:19 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="swaroop",
+    author_email="swaroopyadav49@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
